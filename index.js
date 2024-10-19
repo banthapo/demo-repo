bee7c9ccc1cd96f746ee01243521e69f463ce41e
banthapo <lchijuwa@gmail.com>
2024-10-19 14:12:42 +0200

    we r on trial

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const handleHeadClick = (e) => {
-  //   console.log(e.target);
-};
+async function handleHeadClick(e) {
+  console.log(e.target);
+}
 
-document.getElementById("heading").addEventListener(
-  "click",
-  function (event) {
-    console.log(event.target, " === ", "how");
-  },
-  true
-);
+// document.getElementById("heading").addEventListener(
+//   "click",
+//   function (event) {
+//     console.log(event.target, " === ", "how");
+//   },
+//   true
+// );
