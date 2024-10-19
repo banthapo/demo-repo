e64dde69abfddacc78900b4630478b6cf113c012
banthapo <lchijuwa@gmail.com>
2024-10-19 16:13:04 +0200

    major changes occurred
    we shall see

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-async function handleHeadClick(e) {
-  console.log(e.target);
-}
+document.getElementById("heading").addEventListener(
+  "click",
+  function (event) {
+    console.log(event.target, " === ", "how");
+    axios.get("https://api.github.com/users/hadley/orgs").then((data) => {
+      console.log(data.data);
+    });
+  },
+  true
+);
 
-// document.getElementById("heading").addEventListener(
-//   "click",
-//   function (event) {
-//     console.log(event.target, " === ", "how");
-//   },
-//   true
-// );
+async function handleHeadClick(e) {
+  const data = await axios.get("https://api.github.com/users/hadley/orgs");
+  console.log(data);
+}
