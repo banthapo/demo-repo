df3a0ad9e3b4682a89dd3191c447a291d370e8a3
banthapo <lchijuwa@gmail.com>
2024-10-20 10:53:39 +0200

    deleted module.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-document.getElementById("heading").addEventListener(
-  "click",
-  function (event) {
-    console.log(event.target, " === ", "how");
-    axios.get("https://api.github.com/users/hadley/orgs").then((data) => {
-      console.log(data.data);
-    });
-  },
-  true
-);
+document.getElementById("heading").addEventListener("click", function (event) {
+  console.log(event.target, " === ", "how");
+  axios.get("https://api.github.com/users/hadley/orgs").then((data) => {
+    console.log(data.data);
+  });
+});
 
 async function handleHeadClick(e) {
   const data = await axios.get("https://api.github.com/users/hadley/orgs");
