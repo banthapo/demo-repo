f74fe87ff6366e9008b16ec2d262c7ba5bb1f035
banthapo <lchijuwa@gmail.com>
2024-10-20 12:27:34 +0200

    changed index files.

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-document.getElementById("heading").addEventListener("click", function (event) {
-  console.log(event.target, " === ", "how");
-  axios.get("https://api.github.com/users/hadley/orgs").then((data) => {
-    console.log(data.data);
-  });
-});
+document.getElementById("heading").addEventListener(
+  "click",
+  function (event) {
+    console.log(event.target, " === ", "how");
+    axios.get("https://api.github.com/users/hadley/orgs").then((data) => {
+      console.log(data.data);
+    });
+  },
+  false
+);
 
 async function handleHeadClick(e) {
   const data = await axios.get("https://api.github.com/users/hadley/orgs");
   console.log(data);
 }
+
+const handleGreeting = () => {
+  return console.log("Hello, Lonje.");
+};
+
+handleGreeting();
